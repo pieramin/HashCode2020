423a4af3ea76723cdb7dea9513c183dba5a3d7a1
nicolo <nicolopierami@gmail.com>
2020-02-19 10:02:55 +0100

    Added base class

---
+++ src/base.java
@@ -0,0 +1,7 @@
+public class base {
+
+    //This method prints "Hello Hashcode!" on the screen
+    public static void main(String[] args){
+        System.out.println("Hello Hashcode!");
+    }
+}
